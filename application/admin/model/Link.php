19134c75967fbb2b342fa2e5b193abf1beb0c2d5
dai <admin@shunxin66.com>
2018-07-09 23:10:34 +0800

    完善后台友链管理功能

---
+++ application/admin/model/Link.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @since:  2018/7/9 21:21
+ * @author: daishunxin <admin@shunxin66.com>
+ */
+
+namespace app\admin\model;
+
+use \app\admin\validate\Link as LinkValidate;
+
+class Link extends AdminBase {
+    /**
+     * 获取友链列表
+     * @param $page [当前页]
+     * @param $list_row [每页条数]
+     * @return false|\PDOStatement|string|\think\Collection
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getLinkList($page, $list_row) {
+        return $this->where(['is_del' => 0])->limit(($page - 1) * $list_row, $list_row)->select();
+    }
+
+    /**
+     * 友链添加
+     * @param $data [数据数组]
+     * @return array|bool|string
+     */
+    public function linkAdd($data) {
+        $link_v = new LinkValidate();
+        if (!$link_v->check($data)) {
+            return $link_v->getError();
+        }
+        return $this->save($data) ? true : '数据保存失败';
+    }
+
+    /**
+     * 友链修改
+     * @param $link_id [友链id]
+     * @param $data [数据数组]
+     * @return array|bool|string
+     */
+    public function linkEdit($link_id, $data) {
+        $link_v = new LinkValidate();
+        if (!$link_v->check($data)) {
+            return $link_v->getError();
+        }
+        return $this->where(['is_del' => 0, 'id' => $link_id])->update($data) ? true : '修改失败！';
+    }
+
+    /**
+     * 友链删除
+     * @param $link_id [友链id]
+     * @return bool|string
+     */
+    public function linkDel($link_id) {
+        return $this->where(['id' => $link_id])->update(['is_del' => 1]) ? true : '删除失败！';
+    }
+
+    /**
+     * 获取友链总数
+     * @return int|string
+     */
+    public function getLinkTotal() {
+        return $this->where(['is_del' => 0])->count(1);
+    }
+}
